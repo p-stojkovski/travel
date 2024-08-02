5c862be620d1e69cd9001e7906f2f0bcb9a93c0a
Petar Stojkovski <petars089@gmail.com>
2024-08-02 10:41:42 +0200

    feat(TravelInspiration.API) Improved Error response with the Problem Details RFC

--- TravelInspiration.API/Program.cs
+++ TravelInspiration.API/Program.cs
@@ -5,6 +5,7 @@ using TravelInspiration.API.Features.Itineraries;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services.AddProblemDetails();
 builder.Services.AddHttpClient();
    
 builder.Services.RegisterApplicationServices();
